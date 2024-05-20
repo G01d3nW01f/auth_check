17b897e6c0f46b7246a462a7d7921ec0394472d5
G01d3nW01f <75846902+G01d3nW01f@users.noreply.github.com>
2024-05-20 18:35:22 +0900

    Update auth_check.sh

--- auth_check.sh
+++ auth_check.sh
@@ -9,11 +9,23 @@ MAGENTA='\033[35m'
 CYAN='\033[36m'
 RESET='\033[0m'
 
+echo "-------------------------------------------------------------------"
 
+# initialize
+AUTH_LOG=""
 
-# auto.log path
-AUTH_LOG="/var/log/auth.log"
+if [ -f /var/log/auth.log ]; then
 
+	
+    echo -e "[+]TYPE: ${RED}debian base${RESET}"
+    AUTH_LOG="/var/log/auth.log"
+
+else
+    echo -e "[+]TYPE: ${RED}redhat${RESET}"
+    AUTH_LOG="/var/log/secure"
+fi
+
+echo "-------------------------------------------------------------------"
 
 # IP address regular expression
 PRIVATE_IP_REGEX_V4="^((10\.)|(172\.(1[6-9]|2[0-9]|3[01]))|(192\.168))"
@@ -24,22 +36,22 @@ ipv4s=($(grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' $AUTH_LOG | grep -Ev $PRIVATE_IP
 ipv6s=($(grep -Eo '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}' $AUTH_LOG | grep -Ev $PRIVATE_IP_REGEX_V6 | uniq))
 
 for ipv4 in "${ipv4s[@]}"; do
-	echo -e "${GREEN}[!]IPAddress(ipv4) $ipv4${RESET}"
+	echo -e "${GREEN}[!]IPAddress(ipv4) $ipv4${BLUE}"
     grep $ipv4 $AUTH_LOG
 done
 
 
 for ipv6 in "${ipv6s[@]}"; do
-	echo -e "${GREEN}[!]IPAddress(ipv6) $ipv6${RESET}"
+	echo -e "${GREEN}[!]IPAddress(ipv6) $ipv6${BLUE}"
     grep $ipv6 $AUTH_LOG
 done
 
 
 grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' $AUTH_LOG | grep -E $PRIVATE_IP_REGEX_V4 | uniq | while read -r private_ipv4; do
-echo -e "${BLUE}[!]IPAddress(ipv4) $private_ipv4${RESET}"
+echo -e "${GREEN}[!]IPAddress(ipv4) $private_ipv4${BLUE}"
     grep $private_ipv4 $AUTH_LOG
 done
-
+echo -e "${RESET}"
 
 grep -Eo '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}' $AUTH_LOG | grep -E $PRIVATE_IP_REGEX_V6 | uniq | while read -r private_ipv6; do
 echo -e "${BLUE}[!]IPAddress(ipv6) $private_ipv6${RESET}"
